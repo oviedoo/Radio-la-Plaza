0526c287920a4ffc5f1ee678696ad615a465c06d
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2023-11-30 21:27:59 -0300

    arreglos en el index, dentro del tag style

--- index.php
+++ index.php
@@ -68,8 +68,8 @@
 
         @media screen and (min-width:320px) AND (max-width:767px) {
             #close-button {
-                top: 42px;
-                right: 10px;
+                top: 30px;
+                right: 13px;
             }
         }
     </style>
@@ -77,79 +77,72 @@
 
 <body>
 
-    <body>
+    <div id="video-container">
+        <span id="close-button" onclick="cerrarVideo()">X</span>
+        <video id="video" width="100%" height="100%" autoplay controls ontimeupdate="verificarTiempo()">
+            <source src="./src/assets/videos/luli-montes-coach.mp4" type="video/mp4">
+            Tu navegador no soporta el elemento de video.
+        </video>
+    </div>
 
+    <?php
+    include('./src/views/layouts/header.php');
+    ?>
+    <?php
+    include('./src/views/layouts/reproductor.php');
+    ?>
+    <main id="content-container">
         <?php
-        $videoCookie = isset($_COOKIE['video_played']) ? $_COOKIE['video_played'] : 'false';
-
-        if ($videoCookie !== 'true') {
+        include('./src/views/layouts/publicidades.php');
         ?>
-            <div id="video-container">
-                <span id="close-button" onclick="cerrarVideo()">X</span>
-                <video id="video" autoplay controls ontimeupdate="verificarTiempo()">
-                    <source src="./src/assets/videos/luli-montes-coach.mp4" type="video/mp4">
-                    Tu navegador no soporta el elemento de video.
-                </video>
+        <div id="main">
+            <?php
+            include('./src/views/layouts/noticias.php');
+            ?>
+            <div class="conteiner">
+                <button id="verMas">Ver más</button>
             </div>
+            <script src="./src/js/fuction.js"></script>
+        </div>
         <?php
+        include('./src/views/layouts/publicidades2.php');
+        ?>
+    </main>
+
+    <?php
+    include('./src/views/layouts/footer.php');
+    ?>
+
+    <script src="./src/js/fuction.js"></script>
+    <script>
+        // Función para cerrar el video al alcanzar el minuto 0:33
+        function verificarTiempo() {
+            var video = document.getElementById("video");
+            // Verifica si el tiempo actual del video es mayor o igual a 33 segundos
+            if (video.currentTime >= 33) {
+                cerrarVideo();
+            }
         }
-        ?>
 
-        <?php
-        include('./src/views/layouts/header.php');
-        ?>
-        <?php
-        include('./src/views/layouts/reproductor.php');
-        ?>
-        <main id="content-container">
-            <?php
-            include('./src/views/layouts/publicidades.php');
-            ?>
-            <div id="main">
-                <?php
-                include('./src/views/layouts/noticias.php');
-                ?>
-                <div class="conteiner">
-                    <button id="verMas">Ver más</button>
-                </div>
-                <script src="./src/js/fuction.js"></script>
-            </div>
-            <?php
-            include('./src/views/layouts/publicidades2.php');
-            ?>
-        </main>
+        // Función para cerrar el video
+        function cerrarVideo() {
+            var videoContainer = document.getElementById("video-container");
+            var contentContainer = document.getElementById("content-container");
 
-        <?php
-        include('./src/views/layouts/footer.php');
-        ?>
+            videoContainer.style.display = "none";
+            contentContainer.style.display = "block";
 
-        <script src="./src/js/fuction.js"></script>
-        <script>
-            // Función para cerrar el video al alcanzar el minuto 0:33
-            function verificarTiempo() {
-                var video = document.getElementById("video");
-                // Verifica si el tiempo actual del video es mayor o igual a 33 segundos
-                if (video.currentTime >= 33) {
-                    cerrarVideo();
-                }
-            }
+            var video = document.getElementById("video");
+            video.pause();
+            video.currentTime = 0; // Reinicia la reproducción al principio
+        }
 
-            // Función para cerrar el video
-            function cerrarVideo() {
-                var videoContainer = document.getElementById("video-container");
-                var contentContainer = document.getElementById("content-container");
-
-                videoContainer.style.display = "none";
-                contentContainer.style.display = "block";
-
-                var video = document.getElementById("video");
-                video.pause();
-                video.currentTime = 0; // Reinicia la reproducción al principio
-
-                // Configurar la cookie para indicar que el video ya se reprodujo
-                document.cookie = "video_played=true; expires=Thu, 01 Jan 2022 00:00:00 UTC; path=/";
-            }
-        </script>
-    </body>
+        // Función que se ejecuta cuando se carga el metadato del video
+        document.getElementById("video").addEventListener("loadedmetadata", function() {
+            // Reproducir el video automáticamente al cargar la página
+            this.play();
+        });
+    </script>
+</body>
 
 </html>
