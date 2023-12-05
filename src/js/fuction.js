4c75e060bb01f022a845da68a663d10b2bb3f8df
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2023-12-05 10:25:57 -0300

    se borró el nav dentro de js

--- src/js/fuction.js
+++ src/js/fuction.js
@@ -1,59 +1,4 @@
-(function () {
-  const listElements = document.querySelectorAll(".menu__item--show");
-  const list = document.querySelector(".menu__links");
-  const menu = document.querySelector(".menu__hamburguer");
 
-  const addClick = () => {
-    listElements.forEach((element) => {
-      element.addEventListener("click", () => {
-        let subMenu = element.children[1];
-        let height = 0;
-        element.classList.toggle("menu__item--active");
-
-        if (subMenu.clientHeight === 0) {
-          height = subMenu.scrollHeight;
-        }
-
-        subMenu.style.height = `${height}px`;
-      });
-    });
-  };
-
-  const deleteStyleHeight = () => {
-    listElements.forEach((element) => {
-      if (element.children[1].getAttribute("style")) {
-        element.children[1].removeAttribute("style");
-        element.classList.remove("menu__item--active");
-      }
-    });
-  };
-
-  window.addEventListener("resize", () => {
-    if (window.innerWidth > 800) {
-      deleteStyleHeight();
-      if (list.classList.contains("menu__links--show"))
-        list.classList.remove("menu__links--show");
-    } else {
-      addClick();
-    }
-  });
-
-  if (window.innerWidth <= 800) {
-    addClick();
-  }
-
-  menu.addEventListener("click", () =>
-    list.classList.toggle("menu__links--show")
-  );
-
-  const menuLinks = document.querySelectorAll('.menu__inside a[href^="#"]');
-
-  menuLinks.forEach((menuLink) => {
-    menuLink.addEventListener("click", function () {
-      list.classList.remove("menu__links--show");
-    });
-  });
-})();
 
 //AÑO ACTUAL
 function yearAgo() {
@@ -61,9 +6,10 @@ function yearAgo() {
   const n = d.getFullYear();
 
   document.getElementById("yearAgo").innerText = n;
+  yearAgo();
 }
 
-yearAgo();
+
 
 // BOTON VER MÁS
 document.addEventListener("DOMContentLoaded", function () {
@@ -83,4 +29,6 @@ document.addEventListener("DOMContentLoaded", function () {
     // Ocultar el botón "Ver más" después de mostrar todas las noticias
     verMasButton.style.display = "none";
   });
+
+
 })();
