7fee5ef76ecf9f3216a1471e8f646b847df3d9ba
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2023-12-04 23:45:55 -0300

    se corrigio el navLink del header

--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -3,36 +3,36 @@
     <nav class="menu">
         <section class="menu__container">
             <div class="menu__logo">
-                <a href="./index.php"><img src="./src/assets/img/laplaza.webp" id="logo_bars" /></a>
+                <a href="./index.php"><img src="./src/assets/img/laplaza.webp" alt="Logo" title="Inicio" id="logo_bars" /></a>
             </div>
             <ul class="menu__links">
                 <li class="menu__item">
-                    <a href="./index.php" class="menu__link">Inicio</a>
+                    <a href="./index.php" class="menu__link" alt="Inicio">Inicio</a>
                 </li>
 
                 <li class="menu__item menu__item--show">
-                    <a href="#" class="menu__link">Sobre Nosotros<img src="./src/assets/img/svg/arrow.svg" class="menu__arrow" /></a>
+                    <a href="#" class="menu__link" alt="Sobre Nosotros">Sobre Nosotros<img src="./src/assets/img/svg/arrow.svg" class="menu__arrow" /></a>
                     <ul class="menu__nesting">
                         <li class="menu__inside">
-                            <a href="#main" class="menu__link menu__link--inside">Noticias</a>
+                            <a href="#main" class="menu__link menu__link--inside" alt="Noticias">Noticias</a>
                         </li>
                         <li class="menu__inside">
-                            <a href="./podcast.php" target="_blank" class="menu__link menu__link--inside">Podcasts</a>
+                            <a href="./podcast.php" target="_blank" class="menu__link menu__link--inside" alt="Podcasts">Podcasts</a>
                         </li>
                         <li class="menu__inside">
-                            <a href="./s-n.php" target="_blank" class="menu__link menu__link--inside descarga__app">Quienes somos</a>
+                            <a href="./s-n.php" target="_blank" class="menu__link menu__link--inside descarga__app" alt="Quienes somos">Quienes somos</a>
                         </li>
                     </ul>
                 </li>
 
                 <li class="menu__item menu__item--show">
-                    <a href="#" class="menu__link">Contacto <img src="./src/assets/img/svg/arrow.svg" class="menu__arrow" /></a>
+                    <a href="#" class="menu__link" alt="Contacto">Contacto <img src="./src/assets/img/svg/arrow.svg" class="menu__arrow" /></a>
                     <ul class="menu__nesting">
                         <li class="menu__inside">
-                            <a href="https://wa.me/+543522408620" target="_blank" class="menu__link menu__link--inside">Número Celular: +54 3522-408620</a>
+                            <a href="https://wa.me/+543522408620" target="_blank" class="menu__link menu__link--inside" title="Nuestro número">Número Celular: +54 3522-408620</a>
                         </li>
                         <li class="menu__inside">
-                            <a href="https://www.google.com/maps/place/Belgrano+15,+San+Francisco+del+Cha%C3%B1ar,+C%C3%B3rdoba/@-29.7869453,-63.9411761,21z/data=!4m6!3m5!1s0x9431b7c5e234d7d5:0xf4958e8f854727b7!8m2!3d-29.7869283!4d-63.9410758!16s%2Fg%2F11trhhtqfw?hl=es&entry=ttu" target="_blank" class="menu__link menu__link--inside" title="Mi Direccion">San
+                            <a href="https://www.google.com/maps/place/Belgrano+15,+San+Francisco+del+Cha%C3%B1ar,+C%C3%B3rdoba/@-29.7869453,-63.9411761,21z/data=!4m6!3m5!1s0x9431b7c5e234d7d5:0xf4958e8f854727b7!8m2!3d-29.7869283!4d-63.9410758!16s%2Fg%2F11trhhtqfw?hl=es&entry=ttu" target="_blank" class="menu__link menu__link--inside" title="Nuestra direccion">San
                                 Francisco del Chañar, Córdoba, Argentina
                             </a>
                         </li>
