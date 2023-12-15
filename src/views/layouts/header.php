749f1ac04498496de65bc3975aa80c443b9c4d8e
Jeremías Oviedo <jeremiasoviedo01@gmail.com>
2023-12-15 20:33:17 -0300

    Se modificó en el navbar la seccion contacto

--- src/views/layouts/header.php
+++ src/views/layouts/header.php
@@ -29,7 +29,11 @@
                     <a href="#" class="menu__link" alt="Contacto">Contacto <img src="./src/assets/img/svg/arrow.svg" class="menu__arrow" /></a>
                     <ul class="menu__nesting">
                         <li class="menu__inside">
-                            <a href="https://wa.me/+543522408620" target="_blank" class="menu__link menu__link--inside" title="Nuestro número">Número Celular: +54 3522-408620</a>
+                            <a href="https://wa.me/+543522408620" target="_blank" class="menu__link menu__link--inside" title="Nuestro número">+54 9 3522 40-8620 (AR)</a>
+                        </li>
+                        <li class="menu__inside">
+                            <a href="mailto:laplazacomunit15@gmail.com" target="_blank" class="menu__link menu__link--inside" title="Nuestro Email">laplazacomunit15@gmail.com</a>
+
                         </li>
                         <li class="menu__inside">
                             <a href="https://www.google.com/maps/place/Belgrano+15,+San+Francisco+del+Cha%C3%B1ar,+C%C3%B3rdoba/@-29.7869453,-63.9411761,21z/data=!4m6!3m5!1s0x9431b7c5e234d7d5:0xf4958e8f854727b7!8m2!3d-29.7869283!4d-63.9410758!16s%2Fg%2F11trhhtqfw?hl=es&entry=ttu" target="_blank" class="menu__link menu__link--inside" title="Nuestra direccion">San
